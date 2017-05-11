da68dacd02f5ae311dd340bcd2e7a231ccdd525b
Aleksey <lehazakharov@yandex.ru>
2017-05-11 20:55:59 +0300

    Start Splay Tree implementation without smart ptr

--- src/main.cpp
+++ src/main.cpp
@@ -15,22 +15,18 @@ int main()
 	using std::endl;
 	
 
-	MyBinTree<int> tree;
-	tree.Insert(5);
+	MySplayTree<int> tree;
+	tree.Insert(6);
 	tree.Insert(2);
 	tree.Insert(1);
-	tree.Insert(3);
 	tree.Insert(4);
-	tree.Insert(10);
-	tree.Insert(6);
+	tree.Insert(3);
+	tree.Insert(5);
 	tree.Insert(7);
+	tree.Insert(8);
 	tree.InorderTraverse();
-
-	std::cout << "prer= " << tree.Successor(6) << std::endl;
-
 	
-
-	tree.InorderTraverse();
+	
 	
 
 	return 0;
